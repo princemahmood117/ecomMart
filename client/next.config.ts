8887ce5010d9752bbe1afd54aa0fefb923712e2a
iftekhar <princemahmood117@gmail.com>
2026-07-22 00:14:40 +0600

    links changed

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,8 +3,26 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-       remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
+       remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" },{
+        protocol: "https",
+        hostname: "i.ibb.co",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co.com",
+        pathname: "/**",
+      },],
      },
+
+     async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
